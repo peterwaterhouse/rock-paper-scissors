af29247fc2fb0c043df6d0929537f11cb76a8707
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-12 16:23:20 +1000

    completed project w error checking and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+This project was made as a part of The Odin Project to practice my JavaScript skills - it's a basic Rock Paper Scissors game played in your browser's debugging console.
--- jscript.js
+++ jscript.js
@@ -1,12 +1,49 @@
-console.log(getComputerChoice())
+let humanScore = 0
+let computerScore = 0
+
+playGame()
+
+function getHumanChoice() {
+    humanChoice = prompt("Enter 'rock', 'paper' or 'scissors'")
+    if (humanChoice != null) {
+        humanChoice.toLowerCase()
+        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+            return humanChoice
+        } else {
+            return getHumanChoice()
+        }
+    } else {
+        return getHumanChoice()
+    }
+}
 
 function getComputerChoice() {
     numChoice = Math.floor(Math.random() * 3)
     if (numChoice === 0) {
         return "rock"
     } else if (numChoice === 1) {
-        return "paper" 
+        return "paper"
     } else if (numChoice === 2) {
         return "scissors"
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === "rock" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "paper") || (humanChoice === "scissors" && computerChoice === "scissors")) {
+        return "You both chose " + humanChoice + " - It's a draw!"
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore++;
+        return "You win! " + humanChoice + " beats " + computerChoice
+    } else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
+        computerScore++;
+        return "You lose! " + computerChoice + " beats " + humanChoice
+    }
+}
+
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        console.log(playRound(getHumanChoice(), getComputerChoice()))
+        console.log("Your score: " + humanScore)
+        console.log("Computer score: " + computerScore)
     }
 }
