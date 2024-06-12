b552b8031959baa4f6616b458ce94f53f5d29e54
Peter Waterhouse <87525533+peterwaterhouse@users.noreply.github.com>
2024-06-12 15:55:24 +1000

    added getComputerChoice function

--- jscript.js
+++ jscript.js
@@ -1,1 +1,12 @@
-console.log("test")
+console.log(getComputerChoice())
+
+function getComputerChoice() {
+    numChoice = Math.floor(Math.random() * 3)
+    if (numChoice === 0) {
+        return "rock"
+    } else if (numChoice === 1) {
+        return "paper" 
+    } else if (numChoice === 2) {
+        return "scissors"
+    }
+}
